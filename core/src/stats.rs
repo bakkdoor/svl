865cafa2f3c230954417d437f7a513544a65373e
Christopher Bertels <bakkdoor@flasht.de>
2023-08-20 06:43:49 +0200

    Add HttpStatsClient semaphore

--- core/src/stats.rs
+++ core/src/stats.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::Arc};
 
 use serde_derive::{Deserialize, Serialize};
 
@@ -21,12 +21,46 @@ pub struct Stats {
     pub word_count: usize,
     pub unique_word_count: usize,
     pub unique_words_not_in_other_texts_count: usize,
-
     pub words: HashMap<Word, WordStats>,
+}
+
+impl Stats {
+    pub fn new() -> Self {
+        Stats {
+            word_count: 0,
+            unique_word_count: 0,
+            unique_words_not_in_other_texts_count: 0,
+            words: HashMap::new(),
+        }
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct WordStats {
     pub word: Word,
     pub count: usize,
+}
+
+pub struct HttpStatsClient {
+    client: reqwest::Client,
+    semaphore: Arc<tokio::sync::Semaphore>,
+}
+
+impl HttpStatsClient {
+    const MAX_CONCURRENT_REQUESTS: usize = 10;
+
+    pub fn new() -> crate::Result<Self> {
+        let client = reqwest::Client::builder().https_only(true).build()?;
+        // allow max of MAX_CONCURRENT_REQUESTS concurrent requests using this http client pool
+        let semaphore = Arc::new(tokio::sync::Semaphore::new(Self::MAX_CONCURRENT_REQUESTS));
+
+        Ok(Self { client, semaphore })
+    }
+
+    pub async fn fetch_text(&self, text_url: &str) -> crate::Result<String> {
+        let permit = self.semaphore.acquire().await?;
+        let text = self.client.get(text_url).send().await?.text().await?;
+        drop(permit);
+        Ok(text)
+    }
 }
