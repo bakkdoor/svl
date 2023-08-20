82f2a46bfb87c3936b530ef25b83b072c7391e22
Christopher Bertels <bakkdoor@flasht.de>
2023-08-20 05:54:32 +0200

    Add more stuff, start Error type, add cspell config

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,3 +1,15 @@
+use thiserror::Error;
+
+pub type Result<T> = std::result::Result<T, SVLError>;
+
+#[derive(Error, Debug)]
+pub enum SVLError {
+    #[error("IO error: {0:?}")]
+    IOError(#[from] std::io::Error),
+    #[error("Invalid state")]
+    InvalidState,
+}
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
