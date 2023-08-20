d4ab72b25f720489f15c35bf78b722d57bdc039b
Christopher Bertels <bakkdoor@flasht.de>
2023-08-20 06:04:05 +0200

    Add more deps, update cspell config

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,15 +1,28 @@
 use thiserror::Error;
+use tokio::task::JoinError;
 
 pub type Result<T> = std::result::Result<T, SVLError>;
 
 #[derive(Error, Debug)]
 pub enum SVLError {
+    #[error("invalid header value: {0}")]
+    InvalidHeaderValue(#[from] reqwest::header::InvalidHeaderValue),
+
+    #[error("reqwest error: {0}")]
+    Reqwest(#[from] reqwest::Error),
+
+    #[error("task join error: {0}")]
+    TaskJoin(#[from] JoinError),
+
+    #[error("failed to acquire semaphore permit: {0}")]
+    SemaphoreAcquire(#[from] tokio::sync::AcquireError),
+
     #[error("IO error: {0:?}")]
     IOError(#[from] std::io::Error),
     #[error("Invalid state")]
     InvalidState,
     #[error("Unknown error: {0:?}")]
-    Unknown(Option<String>)
+    Unknown(Option<String>),
 }
 
 pub fn add(left: usize, right: usize) -> usize {
