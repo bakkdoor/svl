db7727b77cdf03f701b5d13c089fb524ef139df3
Christopher Bertels <bakkdoor@flasht.de>
2023-08-20 08:53:41 +0200

    Add HttpStatsClient::get_authors & scraper dependency

--- core/src/client.rs
+++ core/src/client.rs
@@ -1,5 +1,7 @@
 use std::sync::Arc;
 
+use serde_derive::{Deserialize, Serialize};
+
 use crate::text::Text;
 
 #[derive(Debug)]
@@ -25,4 +27,41 @@ impl HttpStatsClient {
         let text = Text::new(text_url.into(), text);
         Ok(text)
     }
+
+    pub async fn get_authors(&self) -> crate::Result<Vec<AuthorInfo>> {
+        let _permit = self.semaphore.acquire().await?;
+        let html_text = self
+            .client
+            .get("https://latin-texts.herokuapp.com/")
+            .send()
+            .await?
+            .text()
+            .await?;
+        // parse html and find tag
+        // <select name="dest" ..>
+        let html = scraper::Html::parse_document(&html_text);
+        let mut authors = Vec::new();
+        for author in html.select(&scraper::Selector::parse("select[name=dest]").unwrap()) {
+            println!("{:?}", author);
+            authors.push(AuthorInfo {
+                name: author.value().attr("value").unwrap().into(),
+                url: author.value().attr("value").unwrap().into(),
+                texts: Vec::new(),
+            });
+        }
+        Ok(authors)
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct AuthorInfo {
+    pub name: String,
+    pub url: String,
+    pub texts: Vec<TextInfo>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct TextInfo {
+    pub name: String,
+    pub url: String,
 }
